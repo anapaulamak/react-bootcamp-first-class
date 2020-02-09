7d4b48b8252a8424b89f0f47fda1b4cc67a1c839
ana paula <anapaulamak@gmail.com>
2020-02-09 11:01:01 -0300

    Add code working

--- first-react-app/src/components.js
+++ first-react-app/src/components.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import { products } from './mock';
+import { PRODUCTS } from './mock';
 
 export class FilterableProductTable extends Component{
   constructor(props){
     super(props);
     this.state = {
-      searchText: '',
-      isChecked: false,
+      filterText: '',
+      inStockOnly: false,
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+
+  handleChange(filterText){
+    this.setState({
+      filterText: filterText
+    })
+  }
+
+  handleCheck(inStockOnly){
+    this.setState({
+      inStockOnly: inStockOnly
+    })
   }
   render(){
-    console.log('prods', products)
     return(
       <div>
-        <SearchBar />
-        <ProductTable products={products}/>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onChange={this.handleChange}
+          onCheck={this.handleCheck}
+        />
+        <ProductTable
+          products={PRODUCTS}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          />
       </div>
     )
   }
@@ -23,52 +45,65 @@ export class FilterableProductTable extends Component{
 export class SearchBar extends Component {
   constructor(props){
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
   }
 
+  handleChange(e){ this.props.onChange(e.target.value); }
+  handleCheck(e){ this.props.onCheck(e.target.checked); }
+
   render(){
+    
     return(
-      <div>
-        <input type="text" placeholder="Seach..."></input>
+      <form>
+        <input
+          type="text"
+          placeholder="Seach..."
+          value={this.props.filterText}
+          onChange={this.handleChange}/>
         <p>
-          <input type="checkbox"></input>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleCheck}/>
           Only show products in stock
         </p>
-      </div>
+      </form>
     )
   }
 }
 
-const ProductRow = (props) => {
-  const product = props.product
-  return(
-    <tr>
-      <td>{product.name}</td>
-      <td>{product.price}</td>
-    </tr>
-  )
-}
-
-const ProductCategoryRow = (props) => {
-  const product = props.product
-  return(
-    <tr>
-      <th colSpan='2'>{product.category}</th>
-    </tr>
-  )
-}
-
 export class ProductTable extends Component{
   render(){
-    const rows = [];
-    var lastCategory = null;
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
 
-    this.props.products.forEach(product => { 
-      if(product.category !== lastCategory) {
-        rows.push(<ProductCategoryRow product={product.category} key={product.category}/>)
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if(product.name.indexOf(filterText) === -1){
+        return;
       }
-      rows.push(<ProductRow product={product} key={product.name}/>)
+      if(inStockOnly && !product.stocked){
+        return;
+      }
+      if(product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            product={product}
+            key={product.category}
+          />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
+      );
       lastCategory = product.category;
-    })
+    });
 
     return(
       <table>
@@ -85,5 +120,27 @@ export class ProductTable extends Component{
       </table>
     )
   }
+}
+
+const ProductRow = (props) => {
+  const product = props.product;
+  return(
+    <tr>
+      {!product.stocked ? 
+        <td style={{color: 'red'}}>{product.name}</td> :
+        <td>{product.name}</td>
+      }
+      <td>{product.price}</td>
+    </tr>
+  )
+}
+
+const ProductCategoryRow = (props) => {
+  const product = props.product;
+  return(
+    <tr>
+      <th colSpan='2'>{product.category}</th>
+    </tr>
+  )
 }
 
--- first-react-app/src/mock.js
+++ first-react-app/src/mock.js
@@ -1,4 +1,4 @@
-export const products = 
+export const PRODUCTS = 
 [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
